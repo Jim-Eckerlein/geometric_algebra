c756e41d631973d6156688756d30aac748ed98f4
Jim Eckerlein <jim.eckerlein@icloud.com>
2022-08-25 13:33:27 +0200

    Generate algebra using build.rs

---
+++ build.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    Command::new("cargo")
+        .current_dir("./codegen")
+        .arg("run")
+        .arg("--")
+        .arg("ppga3d:0,1,1,1;Scalar:1;MultiVector:1,e23,-e13,e12|e0,-e023,e013,-e012|e123,e1,e2,e3|e0123,e01,e02,e03;Rotor:1,e23,-e13,e12;Point:e123,-e023,e013,-e012;Plane:e0,e1,e2,e3;Line:e01,e02,e03|e23,-e13,e12;Translator:1,e01,e02,e03;Motor:1,e23,-e13,e12|e0123,e01,e02,e03;Branch:e01,e02,e03")
+        .spawn()?;
+
+    println!("cargo:rerun-if-changed=build.rs");
+    Ok(())
+}
